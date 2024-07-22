db02ceeeff4930810dd828ce10a1f31c5f09e860
sunflow-er <antjd03170@gmail.com>
2024-07-23 02:46:46 +0900

    웹 요청 실행

--- app/src/main/java/org/javaapp/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/org/javaapp/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package org.javaapp.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.javaapp.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
@@ -24,7 +30,21 @@ class PhotoGalleryFragment : Fragment() {
             .build()
 
         // FlickrApi 인터페이스를 구현한 API 인스턴스 생성
-        val flickrApi : FlickrApi = retrofit.create(FlickrApi::class.java) 
+        val flickrApi : FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        // 실행 가능한 웹 요청을 나타내는 Call 객체 생성
+        val flickrHomePageRequest : Call<String> = flickrApi.fetchContents()
+
+        // 비동기로 Call 객체가 나타내는 웹 요청 실행
+        flickrHomePageRequest.enqueue(object  : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received : ${response.body()}")
+            }ㅛ청
+        })
     }
 
     override fun onCreateView(
