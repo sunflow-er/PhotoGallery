548dcd6a449236a26c8d31669941da7444be08b7
sunflow-er <antjd03170@gmail.com>
2024-07-23 01:10:49 +0900

    Retrofit 인스턴스와 FlickrApi 인스턴스 생성

--- app/src/main/java/org/javaapp/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/org/javaapp/photogallery/PhotoGalleryFragment.kt
@@ -7,9 +7,23 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.javaapp.photogallery.api.FlickrApi
+import retrofit2.Retrofit
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Retrofit 인스턴스 생성
+        val retrofit : Retrofit = Retrofit.Builder() 
+            .baseUrl("https://flickr.com/") // 접속할 엔드포인트의 기본 URL
+            .build()
+
+        // FlickrApi 인터페이스를 구현한 API 인스턴스 생성
+        val flickrApi : FlickrApi = retrofit.create(FlickrApi::class.java) 
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
